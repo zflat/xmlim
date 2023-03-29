d222c574046c69a6d12a63882a3ce6b6dc656ddb
William Wedler <wwedler@riseup.net>
2023-03-29 04:34:23 -0400

    using state-chart-cat for xvg output

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,12 +2,14 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 
 import * as nomnoml from "nomnoml";
+// import {IRenderOptions, render} from "state-machine-cat";
 
 import { Flags } from "@oclif/core";
 
 import { chartFromXml } from "./convert";
 import { format as nomnomlFormat } from "./chartFormat/nomnoml-format";
 import { format as mermaidFormat } from "./chartFormat/mermaid-format";
+import { format as smcatFormat } from "./chartFormat/state-machine-cat-format";
 
 export const watchFlags = {
   format: Flags.string({
@@ -24,21 +26,31 @@ export async function genSingleFile(
   xml: string,
   format: string
 ): Promise<string> {
+  if (format === "mermaid") {
+    return chartFromXml(xml, mermaidFormat);
+  }
+
+  const outputPath = `${path.dirname(xmlFilePath)}/${path
+    .basename(xmlFilePath)
+    .split(".")
+    .slice(0, -1)
+    .join(".")}.svg`;
+
+  let chart = "";
+  let svg = "";
+
   if (format === "svg") {
-    const chart = chartFromXml(xml, nomnomlFormat);
-    const svg = nomnoml.renderSvg(chart);
-    const outputPath = `${path.dirname(xmlFilePath)}/${path
-      .basename(xmlFilePath)
-      .split(".")
-      .slice(0, -1)
-      .join(".")}.svg`;
+    chart = chartFromXml(xml, smcatFormat);
+    const smcat = require("state-machine-cat");
+    svg = smcat.render(chart);
+  }
+
+  if (chart !== "" && svg !== "") {
     fs.writeFile(outputPath, svg, (err) => {
       if (err !== null) {
         throw err;
       }
     });
-  } else if (format === "mermaid") {
-    return chartFromXml(xml, mermaidFormat);
   }
 
   return "";
